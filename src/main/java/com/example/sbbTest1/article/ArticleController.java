c0a12b06e413eb7f59149eb38069a5d6313a5392
Leesamgsoo <sslee3680035@gmail.com>
2024-09-02 10:19:28 +0900

    article list create

--- src/main/java/com/example/sbbTest1/article/ArticleController.java
+++ src/main/java/com/example/sbbTest1/article/ArticleController.java
@@ -2,9 +2,22 @@ package com.example.sbbTest1.article;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/article")
 public class ArticleController {
     private final ArticleService articleService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articleList = this.articleService.list();
+        model.addAttribute("articleList", articleList);
+        return "article_list";
+    }
 }
--- src/main/java/com/example/sbbTest1/article/ArticleService.java
+++ src/main/java/com/example/sbbTest1/article/ArticleService.java
@@ -3,8 +3,14 @@ package com.example.sbbTest1.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    public List<Article> list() {
+        return this.articleRepository.findAll();
+    }
 }
