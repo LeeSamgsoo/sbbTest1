35ab7453601f8ef922bc246c4f96d351fbbc79ab
Leesamgsoo <sslee3680035@gmail.com>
2024-09-13 13:40:50 +0900

    signup form

---
+++ src/main/java/com/example/sbbTest1/user/UserCreateForm.java
@@ -0,0 +1,25 @@
+package com.example.sbbTest1.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+    @NotEmpty(message = "아이디가 비어있습니다.")
+    @Size(min = 3, max = 25, message = "아이디가 너무 짧거나 깁니다.")
+    private String username;
+
+    @NotEmpty(message = "비밀번호가 비어있습니다.")
+    private String password;
+
+    @NotEmpty(message = "비밀번호 확인이 비어있습니다.")
+    private String passwordCheck;
+
+    @NotEmpty(message = "이메일이 비어있습니다.")
+    @Email
+    private String email;
+}
